2a32c1991489e926beda7d13d88728e9015fc050
Alex <bribribri@rambler.ru>
2019-10-10 15:39:47 +0300

    added text-editor-synonyms

--- projects/demo/src/app/modules/text-editor/text-editor-synonyms_/text-editor-synonyms.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextEditorSynonymsDirective } from './text-editor-synonyms.directive';
-
-describe('TextEditorSynonymsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextEditorSynonymsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
